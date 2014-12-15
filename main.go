3941f144ca180d3ae5b65fca4de2bdaafa1c1680
reus <reusee@gmail.com>
2014-12-15 21:03:52 +0800

    implement

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Printf("usage: %s [profile file]", os.Args[0])
+		return
+	}
+	content, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	entries := strings.Split(string(content), "\n")[1:]
+	fileLines := make(map[string][]string)
+	for _, entry := range entries {
+		entry = strings.TrimSpace(entry)
+		if !strings.HasSuffix(entry, " 0") {
+			continue
+		}
+		parts := strings.Split(entry, ":")
+		path := parts[0]
+		if path[0] == '_' {
+			path = path[1:]
+		}
+		lines, ok := fileLines[path]
+		if !ok {
+			content, err = ioutil.ReadFile(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			lines = strings.Split(string(content), "\n")
+			fileLines[path] = lines
+		}
+		parts = strings.SplitN(parts[1], ".", 2)
+		start, err := strconv.Atoi(parts[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.HasSuffix(strings.TrimSpace(lines[start-1]), "NOCOVER") {
+			continue
+		}
+		fmt.Printf("%s : %d\n", path, start)
+	}
+}
